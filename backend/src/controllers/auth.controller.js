cc67aba34de8ab647e5694569d91daddc036a0cf
Ehsan ullah <emehsaan.76@gmail.com>
2025-06-04 16:59:56 +0500

    MongoDB Connection

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("Success")
+}
+
+export const login = (req, res) => {
+    res.send("Success")
+}
+
+export const logout = (req, res) => {
+    res.send("Success")
+}

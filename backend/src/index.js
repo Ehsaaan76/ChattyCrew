75b34118f0bb322d944f37abcf65b3ea0bae047d
Ehsan ullah <emehsaan.76@gmail.com>
2025-06-11 15:54:04 +0500

    signup page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js"
+import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", Credentials: true }));
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-app.listen(port || 5001, ()=>{
-    console.log(`App is listening ${port}`)
-    connectDB();
-})
+app.listen(port || 5001, () => {
+  console.log(`App is listening ${port}`);
+  connectDB();
+});
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Navbar = () => {
+  const { authUser } = useAuthStore();
   return (
     <div>Navbar</div>
   )
